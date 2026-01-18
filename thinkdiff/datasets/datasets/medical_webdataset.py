bb1764a19c0f53a0165c6ea18bb916b6f2d2bb65
Wentao Li <149033035+WentaoLi-CV@users.noreply.github.com>
2026-01-18 17:11:00 +0800

    Update medical_webdataset.py

--- thinkdiff/datasets/datasets/medical_webdataset.py
+++ thinkdiff/datasets/datasets/medical_webdataset.py
@@ -1,3 +1,5 @@
+import json
+
 import webdataset as wds
 
 from thinkdiff.datasets.datasets.base_dataset import BaseDataset
@@ -12,23 +14,17 @@ class MedicalWebDataset(BaseDataset):
             wds.tarfile_to_samples(handler=wds.warn_and_continue),
             wds.shuffle(1000, handler=wds.warn_and_continue),
             wds.decode("pilrgb", handler=wds.warn_and_continue),
-            wds.to_tuple("__key__", "jpg", "json", handler=wds.warn_and_continue),
-            wds.map_tuple(
-                lambda key: key,
-                self.vis_processor,
-                lambda meta: meta,
-                handler=wds.warn_and_continue,
-            ),
+            wds.to_tuple("jpg", "json", handler=wds.warn_and_continue),
+            wds.map_tuple(self.vis_processor, handler=wds.warn_and_continue,),
             wds.map(self.to_dict, handler=wds.warn_and_continue),
         )
 
     def to_dict(self, sample):
-        sample_id = sample[2].get("sample_id", sample[0])
         return {
-            "sample_id": sample_id,
+            "sample_id": sample[0],
             "image": sample[1],
             "answer": self.text_processor(sample[2]["caption"]),
-            "entities": sample[2].get("entities", []),
+            "entities": json.dumps(sample[2].get("entities", []), ensure_ascii=False),
             "modality": sample[2].get("modality"),
             "modality_id": sample[2].get("modality_id"),
         }
